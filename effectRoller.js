58c5dadeb61704fa7c915db3b381eec4897c08b5
Taureon <45183108+Taureon@users.noreply.github.com>
2023-09-23 15:57:30 +0200

    Update effectRoller.js

--- effectRoller.js
+++ effectRoller.js
@@ -1,77 +1,80 @@
-// Stronger:
+// You can do '!roll [id]' in chat to roll an effect.
+// 'id' parameter requires 'chooseRoll' permission.
+// Cooldown of 30 seconds after rolling.
+// Cooldown bypass requires 'infiniteRolls' permission.
+
+// Stronger [0]:
 //     Sets all your stats to 15.
 //     Lasts 20 seconds.
 //     Makes you rainbow colored.
 //
-// Sidewinder:
-//     Summons a Stronger Sidewinder Snake when you left click, which has a 3 second cooldown
+// Boulder [1]:
+//     Makes you ten times as immovable.
 //     Lasts 20 seconds.
 //
-// Hedgehog:
+// Hedgehog [2]:
 //     Increases your max speed and acceleration by 400%.
 //     Lasts 20 seconds.
 //
-// Focus:
+// Focus [3]:
 //     Decreases spread by 50% and all your guns shoot forward.
-//     Lasts 10 seconds.
+//     Lasts 20 seconds.
 //
-// Machine Gun:
+// Machine Gun [4]:
 //     Applies g.mach to your guns.
 //     Lasts 20 seconds.
 //
-// Damage Sponge:
+// Damage Sponge [5]:
 //     Increases your Max Health and Shield by 400%.
 //     Lasts 10 seconds.
 //
-// Vanish:
-//     Makes you invisible.
-//     Lasts 20 seconds.
-//
-// Ant:
+// Ant [6]:
 //     Makes you 75% smaller.
 //     Lasts 15 seconds.
 //
-// Carrot:
+// Carrot [7]:
 //     Increase your FOV by 100%
 //     Lasts 20 seconds.
 
-// Growth Annihilator:
+// Growth Annihilator [8]:
 //     Spawns a Stronger LVL 250 Growth Annihilator, which shoots you once from 10-20 players of distance away and then despawns.
 //
-// No Health:
+// No Health [9]:
 //     Sets your shield to 0 and sets your health to 1% of max health.
 //
-// Slippery:
+// Slippery [10]:
 //     Decreases your acceleration by 66% and increases your max speed by 50%.
 //     Lasts 20 seconds.
 //
-// Vulnerable:
+// Vulnerable [11]:
 //     Decreases your Max Health and Shield by 80%.
 //     Lasts 10 seconds.
 //
-// Black Hole:
+// Black Hole [12]:
 //     Movable entities near you get pulled towards you.
 //     Lasts 15 seconds.
 //
-// Time Bomb:
-//     Makes you explode in 10 seconds, killing you and damaging nearby entities.
+// Old Age [13]:
+//     Kills you in 20 seconds.
 //
-// Balloon:
+// Balloon [14]:
 //     Makes you 300% larger.
 //     Lasts 15 seconds.
 //
-// Blind:
+// Blind [15]:
 //     Decreases your fov by 80%.
 //     lasts 10 seconds.
 
-let { mach: gMach } = require('../gunvals.js'),
+let { combineStats } = require('../facilitators.js'),
+    g = require('../gunvals.js'),
 
 effects = [{
     name: 'Stronger',
-    splash: 'I feel very strong...',
+    splash: 'I feel powerful...',
     duration: 20,
     run: body => {
         let colorOld = body.color,
+            pointsOld = body.skill.points,
             rawOld = body.skill.raw.map(x=>x),
             capsOld = body.skill.caps.map(x=>x),
             stronk = Array(10).fill(15);
@@ -83,6 +86,7 @@ effects = [{
             body.skill.setCaps(capsOld);
             body.skill.set(rawOld);
             body.color = colorOld;
+            body.skill.points = pointsOld;
         }, 20 * 1000);
     },
     statusEffect: new StatusEffect(20 * 30, undefined, body => {
@@ -91,40 +95,54 @@ effects = [{
         e.define('genericEntity');
         e.velocity.x = 5 * Math.sin(ang);
         e.velocity.y = 5 * Math.cos(ang);
-        e.SIZE = body.SIZE;
-        e.coreSize = body.coreSize;
+        e.SIZE = body.size;
+        e.team = body.team;
+        e.color = getTeamColor(body.team);
         e.alpha = 0.5;
         setTimeout(() => e.kill(), 100);
     })
 },
 
 {
-    name: 'Sidewinder',         
-    splash: 'Press Alt-Fire to launch a Snake.',
-    duration: 20
-    //TODO: add invisible sidewinder barrel
+    name: 'Boulder',         
+    splash: 'I am become wall',
+    duration: 20,
+    statusEffect: new StatusEffect(20 * 30, { pushability: 0.1 })
 },
 
 {
     name: 'Hedgehog',           
     splash: 'Gotta go fast!',
-    duration: 20
+    duration: 20,
     statusEffect: new StatusEffect(20 * 30, { acceleration: 5, topSpeed: 5 })
 },
 
 {
     name: 'Focus',              
     splash: 'I CAN AIM.',
-    duration: 10,
+    duration: 20,
     run: body => {
-        //TODO: undo it after effect ends
+        let remember = {}
         for (let gun of body.guns) {
-            let gun;
-            for (let stat in gMach) {
-                gun.settings[stat].angle = 0;
-                gun.settings[stat].spray *= 0.5;
+            remember[gun.id] = {
+                angle: gun.angle,
+                direction: gun.direction
+            }
+            gun.angle = 0;
+            gun.direction = 0;
+            if (gun.settings) {
+                gun.settings.spray *= 0.5;
             }
         }
+        setTimeout(() => {
+            for (let gun of body.guns) {
+                gun.angle = remember[gun.id].angle;
+                gun.direction = remember[gun.id].direction;
+                if (gun.settings) {
+                    gun.settings.spray /= 0.5;
+                }
+            }
+        }, 20 * 1000);
     }
 },
 
@@ -133,14 +151,24 @@ effects = [{
     splash: 'I CANNOT AIM!',
     duration: 20,
     run: body => {
-        //TODO: undo it after effect ends
         for (let gun of body.guns) {
-            let gun;
-            for (let stat in gMach) {
-                gun.settings[stat] *= gMach[stat];
+            if (gun.settings) {
+                for (let stat in g.mach) {
+                    gun.settings[stat] *= g.mach[stat];
+                }
+                gun.trueRecoil *= g.mach.recoil;
             }
-            gun.trueRecoil = gMach.recoil;
         }
+        setTimeout(() => {
+            for (let gun of body.guns) {
+                if (gun.settings) {
+                    for (let stat in g.mach) {
+                        gun.settings[stat] /= g.mach[stat];
+                    }
+                    gun.trueRecoil /= g.mach.recoil;
+                }
+            }
+        }, 20 * 1000);
     }
 },
 {
@@ -167,20 +195,18 @@ effects = [{
 {
     name: 'Growth Annihilator', 
     splash: 'WATCH OUT!!',
-    endNotification: false,
+    noEndNotification: true,
     run: body => {
         let angle = Math.random() * 2 * Math.PI,
         anni = new Entity({
-            x: body.x - Math.sin(angle) * 100,
-            y: body.y - Math.cos(angle) * 100
-        })
+            x: body.x - Math.sin(angle) * 400,
+            y: body.y - Math.cos(angle) * 400
+        });
         anni.define('plugin_effectRoller_strongerGrowthAnnihilator');
+        anni.define({ CONTROLLERS: [["plugin_effectRoller_lookAtEntity", { entity: body }]] });
         anni.facing = angle;
-        setTimeout(() => {
-            for (let i = 0; i < this.guns.length; i++) {
-                this.guns.autofire = true;
-            }
-        }, 2000);
+        anni.team = TEAM_ROOM;
+        anni.color = getTeamColor(TEAM_ROOM);
         setTimeout(() => anni.kill(), 3000);
     }
 },
@@ -188,9 +214,9 @@ effects = [{
 {
     name: 'No Health',          
     splash: 'Oh..',
-    endNotification: false,
+    noEndNotification: true,
     run: body => {
-        body.shield.amount = 0;
+        body.shield.amount = 0.001;
         body.health.amount = body.health.max / 100;
     }
 },
@@ -216,23 +242,23 @@ effects = [{
     statusEffect: new StatusEffect(15 * 30, undefined, body => {
         for (let i = 0; i < entities.length; i++) {
             let entity = entities[i];
-            if (entity.id == body.id || !entity.PUSHABILITY) continue;
-            let angle = Math.atan2(entity.y - body.y, entity.x - body.x),
-                force = entity.PUSHABILITY * 100 / ((entity.y - body.y) ** 2 + (entity.x - body.x) ** 2);
-            entity.accel.x += force * Math.sin(angle);
-            entity.accel.y += force * Math.cos(angle);
+            if (entity.pushability) {
+                let diffX = entity.x - body.x,
+                    diffY = entity.y - body.y,
+                    force = 2500 * entity.pushability / Math.max(1, diffX ** 2 + diffY ** 2);
+                entity.velocity.x -= diffX * force;
+                entity.velocity.y -= diffY * force;
+            }
         }
     })
 },
 
 {
-    name: 'Time Bomb',          
-    splash: 'I must sacrifice myself suddenly...',
-    endNotification: false,
-    run: body => {
-        // TODO: put bomb on player
-        socket.talk('m', name + ' is about to detonate in ' + 10 + ' seconds!');
-        setTimeout(()=> body.kill(), 10 * 1000);
+    name: 'Old Age',          
+    splash: 'Oh no where is my life support???',
+    noEndNotification: true,
+    run: (body, socket) => {
+        setTimeout(()=> body.kill(), 20 * 1000);
     }
 },
 
@@ -250,38 +276,89 @@ effects = [{
     statusEffect: new StatusEffect(10 * 30, { fov: 0.2 })
 }];
 
+class io_plugin_effectRoller_lookAtEntity extends IO {
+    constructor(body, { entity } = {}) {
+        super(body);
+        this.entity = entity;
+    }
+    think() {
+        return {
+            target: {
+                x: this.entity.x - this.body.x,
+                y: this.entity.y - this.body.y
+            }
+        }
+    }
+}
+ioTypes.plugin_effectRoller_lookAtEntity = io_plugin_effectRoller_lookAtEntity;
+
 module.exports = ({ Class, Config, Events }) => {
 
     Class.plugin_effectRoller_strongerGrowthAnnihilator = {
-        PARENT: ["annihilator"],
-        DANGER: 10,
+        PARENT: ["genericTank"],
+        LABEL: "Annihilator",
         SKILL_CAP: Array(10).fill(15),
         SKILL: Array(10).fill(15),
         LEVEL_CAP: 250,
-        LEVEL: 250
+        LEVEL: 250,
+        DANGER: 10,
+        GUNS: [{
+            POSITION: {
+                LENGTH: 20.5,
+                WIDTH: 19.5,
+                DELAY: 1
+            },
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.pound, g.destroy, g.anni]),
+                TYPE: ["bullet", { PERSISTS_AFTER_DEATH: true }],
+                AUTOFIRE: true
+            }
+        }]
     };
 
+    let recent = {},
+        regex = /^[!\/$]roll ?(\d*)?$/i,
+        rollCooldown = 30_000;
+
     Events.on('chatMessage', ({message, socket}) => {
-        if (!/[!\/$]roll/.test(message)) return;
+
+        if (!regex.test(message)) return;
 
         let body = socket.player.body,
-            effect = effect[Math.floor(Math.random() * effects.length)];
+            perms = socket.permissions;
+
+        if (!(perms && perms.infiniteRolls)) {
+            if (recent[body.id]) {
+                socket.talk('m', `You need to wait for ${Math.ceil((recent[body.id] - Date.now()) / 1000)} seconds to roll again!`);
+                return;
+            }
+            
+            recent[body.id] = Date.now() + rollCooldown;
+            setTimeout(() => {
+                delete recent[body.id];
+            }, rollCooldown);
+        }
+
+        let match = (perms && perms.chooseRoll) ? message.match(regex) : null,
+            effectId = match && !isNaN(match[1]) ? parseInt(match[1]) : Math.floor(Math.random() * effects.length),
+            effect = effects[effectId];
 
         if (effect.splash) {
             socket.talk('m', effect.splash);
         }
 
-        socket.talk('m', effect.name);
+        util.log(`${body.name} [${body.id}] rolled effect: ${effect.name} [${effectId}]`);
+        socket.talk('m', 'Rolled: ' + effect.name);
 
         if (effect.statusEffect) {
             body.addStatusEffect(effect.statusEffect);
         }
 
         if (effect.run) {
-            effect.run(body);
+            effect.run(body, socket);
         }
 
-        if (effect.endNotification) {
+        if (!effect.noEndNotification) {
             setTimeout(() => {
                 socket.talk('m', effect.name + ' is about to end in 5 seconds!');
             }, effect.duration * 1000 - 5000);
