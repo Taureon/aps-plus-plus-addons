0c638d03beabd60abdab2788516e3ef7cffc325f
Taureon <45183108+Taureon@users.noreply.github.com>
2023-09-29 08:57:35 +0200

    better alternating frags

--- 4th_of_july.js
+++ 4th_of_july.js
@@ -2,9 +2,10 @@ module.exports = ({ Class, Events }) => {
     return console.log('\u001b[31mADDON 4th_of_july.js IS STILL IN BETA, DO NOT USE IN PRODUCTION\u001b[0m');
 
     let explosions = [];
-    for (let COLOR of ['purple', 'lavender', 'gold', 'black', 'gray', 'white', 'animatedBlueRed', 'animatedRedBlue', 'red', 'blue']) {
-        for (let SHAPE of [0, 3, 4, 5, 6]) {
-            explosions.push(Class[`plugin_4thOfJuly_${SHAPE}_${COLOR}_explosion`] = {
+    for (let SHAPE of [0, 3, 4, -5]) {
+        let current = [];
+        for (let COLOR of ['purple', 'lavender', 'gold', 'black', 'gray', 'white', 'animatedBlueRed', 'animatedRedBlue', 'red', 'blue']) {
+            current.push(Class[`plugin_4thOfJuly_${SHAPE}_${COLOR}_explosion`] = {
                 LABEL: "Explosion",
                 ACCEPTS_SCORE: false,
                 SIZE: 3,
@@ -22,28 +23,29 @@ module.exports = ({ Class, Events }) => {
                 SHAPE, COLOR
             });
         }
+        explosions.push(current);
     }
 
-    Events.on('spawn', (entity) => {
-        entity.on('define', (projectile) => {
+    Events.on('spawn', projectile => {
+        projectile.on('define', set => {
             if (!["bullet", "drone", "minion", "swarm", "trap"].includes(projectile.type)) return;
 
             projectile.on('dead', () => {
-                let count = Math.floor(Math.random() * 10);
-                let fireworkTypesCount = Math.max(1, Math.floor(Math.random() * 4));
-                let fireworkTypes = []
-                for (let i = 0; i < fireworkTypesCount; i++) {
-                    fireworkTypes.push(ran.choose(explosions))
-                }
+                let ticker = true,
+                    count = Math.ceil(Math.random() * 31),
+                    fragsToUse = explosions[Math.floor(Math.random() * explosions.length)],
+                    first = fragsToUse[Math.floor(Math.random() * fragsToUse.length)],
+                    second = fragsToUse[Math.floor(Math.random() * fragsToUse.length)],
+                    angleStart = Math.random() * Math.PI * 2;
+
                 for (let i = 0; i < count; i++) {
-                    let ticker = 0
-                    let angle = (i * (Math.PI * 2)) / count,
+                    let angle = angleStart + (i * (Math.PI * 2)) / count,
                     o = new Entity(projectile);
-                    o.define(fireworkTypes[ticker], false);
-                    ticker >= fireworkTypes.length ? ticker = 0 : ticker++
-                    o.define({ BODY: { DAMAGE: projectile.DAMAGE * 2, HEALTH: projectile.HEALTH * 2 } }, false);
+                    o.define((ticker = !ticker) ? first : second, false);
+                    o.define({ BODY: { DAMAGE: projectile.DAMAGE, HEALTH: projectile.HEALTH} }, false);
                     o.velocity.x = 10 * Math.sin(angle);
                     o.velocity.y = 10 * Math.cos(angle);
+                    o.facing = angle;
                     o.team = projectile.team;
                     o.life();
                 }
