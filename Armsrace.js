a72cdafd550be499bae680dcbc4baca627d5cdab
Twilight <146891309+Lexie-178@users.noreply.github.com>
2023-10-05 17:18:58 -0400

    Update Armsrace.js

--- Armsrace.js
+++ Armsrace.js
@@ -1,1 +1,47 @@
-//Ill do this later...
+exports.quadtwin = {
+   PARENT: [exports.genericTank],
+   LABEL: 'Quad Twin',
+   GUNS: [ {
+         POSITION: [ 20, 8, 1, 0, -5.5, 0, 0, ],
+         PROPERTIES: {
+            SHOOT_SETTINGS: combineStats([g.basic]),
+            TYPE: exports.bullet,
+         }, }, {
+         POSITION: [ 20, 8, 1, 0, -5.5, -90, 0, ],
+         PROPERTIES: {
+            SHOOT_SETTINGS: combineStats([g.basic]),
+            TYPE: exports.bullet,
+         }, }, {
+         POSITION: [ 20, 8, 1, 0, -5.5, 180, 0, ],
+         PROPERTIES: {
+            SHOOT_SETTINGS: combineStats([g.basic]),
+            TYPE: exports.bullet,
+         }, }, {
+         POSITION: [ 20, 8, 1, 0, -5.5, 90, 0, ],
+         PROPERTIES: {
+            SHOOT_SETTINGS: combineStats([g.basic]),
+            TYPE: exports.bullet,
+         }, }, {
+         POSITION: [ 20, 8, 1, 0, 5.5, 0, 0, ],
+         PROPERTIES: {
+            SHOOT_SETTINGS: combineStats([g.basic]),
+            TYPE: exports.bullet,
+         }, }, {
+         POSITION: [ 20, 8, 1, 0, 5.5, 90, 0, ],
+         PROPERTIES: {
+            SHOOT_SETTINGS: combineStats([g.basic]),
+            TYPE: exports.bullet,
+         }, }, {
+         POSITION: [ 20, 8, 1, 0, 5.5, 180, 0, ],
+         PROPERTIES: {
+            SHOOT_SETTINGS: combineStats([g.basic]),
+            TYPE: exports.bullet,
+         }, }, {
+         POSITION: [ 20, 8, 1, 0, 5.5, -90, 0, ],
+         PROPERTIES: {
+            SHOOT_SETTINGS: combineStats([g.basic]),
+            TYPE: exports.bullet,
+         }, }, 
+     ],
+};
+
