10bcad85a4b46ff0c827af661dc138d8064fead0
Taureon <45183108+Taureon@users.noreply.github.com>
2023-10-06 00:44:02 +0200

    compression

--- Armsrace.js
+++ Armsrace.js
@@ -8,47 +8,30 @@ module.exports = ({ Class }) => {
         LABEL: 'Quad Twin',
         GUNS: [ {
             POSITION: [ 20, 8, 1, 0, -5.5, 0, 0, ],
-            PROPERTIES: {
-                SHOOT_SETTINGS: combineStats([g.basic, g.twin]),
-                TYPE: 'bullet',
-            }, }, {
+            PROPERTIES: { SHOOT_SETTINGS: combineStats([g.basic, g.twin]), TYPE: 'bullet' },
+        }, {
             POSITION: [ 20, 8, 1, 0, -5.5, -90, 0, ],
-            PROPERTIES: {
-                SHOOT_SETTINGS: combineStats([g.basic, g.twin]),
-                TYPE: 'bullet',
-            }, }, {
+            PROPERTIES: { SHOOT_SETTINGS: combineStats([g.basic, g.twin]), TYPE: 'bullet' },
+        }, {
             POSITION: [ 20, 8, 1, 0, -5.5, 180, 0, ],
-            PROPERTIES: {
-                SHOOT_SETTINGS: combineStats([g.basic, g.twin]),
-                TYPE: 'bullet',
-            }, }, {
+            PROPERTIES: { SHOOT_SETTINGS: combineStats([g.basic, g.twin]), TYPE: 'bullet' },
+        }, {
             POSITION: [ 20, 8, 1, 0, -5.5, 90, 0, ],
-            PROPERTIES: {
-                SHOOT_SETTINGS: combineStats([g.basic, g.twin]),
-                TYPE: 'bullet',
-            }, }, {
+            PROPERTIES: { SHOOT_SETTINGS: combineStats([g.basic, g.twin]), TYPE: 'bullet' },
+        }, {
             POSITION: [ 20, 8, 1, 0, 5.5, 0, 0, ],
-            PROPERTIES: {
-                SHOOT_SETTINGS: combineStats([g.basic, g.twin]),
-                TYPE: 'bullet',
-            }, }, {
+            PROPERTIES: { SHOOT_SETTINGS: combineStats([g.basic, g.twin]), TYPE: 'bullet' },
+        }, {
             POSITION: [ 20, 8, 1, 0, 5.5, 90, 0, ],
-            PROPERTIES: {
-                SHOOT_SETTINGS: combineStats([g.basic, g.twin]),
-                TYPE: 'bullet',
-            }, }, {
+            PROPERTIES: { SHOOT_SETTINGS: combineStats([g.basic, g.twin]), TYPE: 'bullet' },
+        }, {
             POSITION: [ 20, 8, 1, 0, 5.5, 180, 0, ],
-            PROPERTIES: {
-                SHOOT_SETTINGS: combineStats([g.basic, g.twin]),
-                TYPE: 'bullet',
-            }, }, {
+            PROPERTIES: { SHOOT_SETTINGS: combineStats([g.basic, g.twin]), TYPE: 'bullet' },
+        }, {
             POSITION: [ 20, 8, 1, 0, 5.5, -90, 0, ],
-            PROPERTIES: {
-                SHOOT_SETTINGS: combineStats([g.basic, g.twin]),
-                TYPE: 'bullet',
-            }, }, 
-        ],
+            PROPERTIES: { SHOOT_SETTINGS: combineStats([g.basic, g.twin]), TYPE: 'bullet' },
+        }]
     };
     Class.autotripletwin = makeAuto(Class.tripletwin);
-    Class.tripletwin.UPGRADES_TIER_4 = ["quadtwin", "autotripletwin"]
+    Class.tripletwin.UPGRADES_TIER_4 = ["quadtwin", "autotripletwin"];
 };
