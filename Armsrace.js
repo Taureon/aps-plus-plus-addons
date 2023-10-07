1b5dc79645f133cd09ca55f27e3003b70c55bf99
Taureon <45183108+Taureon@users.noreply.github.com>
2023-10-07 10:13:26 +0200

    specified

--- Armsrace.js
+++ Armsrace.js
@@ -1,6 +1,6 @@
 //twilight :)
 
-// note from trplnr: please dont use glitch or github's text editor
+// note from trplnr: please dont use glitch or github.com's text editor
 // use https://github.dev/Taureon/aps-plus-plus-addons/blob/main/Armsrace.js if you dont plan on getting a code editor
 // im telling u this bcuz syntax errors
 
