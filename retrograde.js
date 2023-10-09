57978c436e7489b1b80531c9d368d38008ea3020
Zenphia <93544652+TGS5994@users.noreply.github.com>
2023-10-09 15:48:30 +0000

    Update retrograde.js

--- retrograde.js
+++ retrograde.js
@@ -1,8 +1,3 @@
-// An addon is guaranteed to run only after all groups are loaded.
-// This is helpful, if your group relies on all other definitions already being loaded.
-// Addons that are dependant on other addons should be named something like
-// "[PARENT ADDON NAME]-[EXTENSION NAME].js", to make sure that it would run after that addon ran.
-
 const { combineStats, addBackGunner } = require('../facilitators.js');
 const { base, gunCalcNames, statnames } = require('../constants.js');
 const g = require('../gunvals.js');
