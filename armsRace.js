71f59f9fbe5b02f878d2b06f28f1d28225fc1d5d
Twilight <146891309+Lexie-178@users.noreply.github.com>
2023-11-01 06:36:31 -0400

    finished medic upgrades

--- armsRace.js
+++ armsRace.js
@@ -1626,6 +1626,36 @@ Class.injection = {
     ],
     STAT_NAMES: statnames.heal,
 };
+    Class.actuary = {
+    PARENT: ["genericTank"],
+    LABEL: "Actuary",
+    TURRETS: [{
+            /** SIZE     X       Y     ANGLE    ARC */
+            POSITION: [13, 0, 0, 0, 360, 1],
+            TYPE: "healerSymbol",},
+    ],
+    GUNS: [{
+            /*** LENGTH  WIDTH   ASPECT    X       Y     ANGLE   DELAY */
+            POSITION: [8, 9, -0.5, 12.5, 0, 0, 0],},{
+            POSITION: [18, 10, 1, 0, 0, 0, 0],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.healer]),
+                TYPE: "healerBullet",},},{
+            /*** LENGTH  WIDTH   ASPECT    X       Y     ANGLE   DELAY */
+            POSITION: [8, 9, -0.5, 13.25, 0, 0, 0],},{
+            POSITION: [18, 10, 1, 1, 0, 0, 0.2],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.healer]),
+                TYPE: "healerBullet",},},{
+            /*** LENGTH  WIDTH   ASPECT    X       Y     ANGLE   DELAY */
+            POSITION: [8, 9, -0.5, 15, 0, 0, 0],},{
+            POSITION: [18, 10, 1, 2, 0, 0, 0.4],
+            PROPERTIES: {
+                SHOOT_SETTINGS: combineStats([g.basic, g.healer]),
+                TYPE: "healerBullet",},},
+    ],
+    STAT_NAMES: statnames.heal,
+};
     Class.avian = makeBird(Class.single, "Avian");
     Class.assistant = makeHybrid(Class.single, "Assistant");
     Class.autoSingle = makeAuto(Class.single);
@@ -1656,6 +1686,6 @@ Class.injection = {
                                                        "autoBonker", "autoBanger", "autoDrifter"]
                     Class.landmine.UPGRADES_TIER_4 = ["limpet", "megaLandmine", "claymore", "autoLandmine", "decoy", "tripwire", "vessel"]
                     Class.banger.UPGRADES_TIER_4 = ["slammer", "megaBanger", "prick", "autoBanger", "tripwire", "thwacker", "sharper"]
-                    Class.medic.UPGRADES_TIER_4 = ["medic", "ointment", "injection"]
+                    Class.medic.UPGRADES_TIER_4 = ["medic", "ointment", "injection", "actuary"]
                                                       
 };
