697f6a037bebc7923028b53e7855a02a5363a4e6
Aydan <148878657+AydanL@users.noreply.github.com>
2024-02-02 17:07:52 +0100

    Update gats.js
    machine guns balance was ass

--- gats.js
+++ gats.js
@@ -9,11 +9,11 @@ const g = {
 // Bases
     gun: { reload: 18, recoil: 1.4, shudder: 0.01, damage: 0.75, speed: 9, spray: 15, size: 0.4, range: 0.5 },
     pistol: { reload: 0.8, recoil: 0.1, damage: 0.4, spray: 0.9, size: 0.5, range: 0.8 },
-    smg: { reload: 0.2, recoil: 0.1, damage: 0.1, spray: 25, range: 0.4 },
+    smg: { reload: 0.15, recoil: 0.1, damage: 0.2, spray: 25, range: 0.4 },
     shotgun: { reload: 2.5, recoil: 0.8, damage: 0.3, spray: 15, size: 0.5 , range: 0.6},
-    assault: { reload: 0.3, recoil: 0.2, damage: 0.1, spray: 5, range: 0.6 },
+    assault: { reload: 0.25, recoil: 0.2, damage: 0.1, spray: 5, range: 0.6 },
     sniper: { reload: 3, recoil: 1, damage: 1, spray: 0.1, speed: 2 },
-    lmg: { reload: 0.4, recoil: 0.2, damage: 0.3, spray: 10, range: 0.8, },
+    lmg: { reload: 0.3, recoil: 0.2, damage: 0.1, spray: 10, range: 0.8, },
     bulletCase: { range: 0.2, speed: 0.3, recoil: 0 },
 };
 Class.gatsBullet = {
