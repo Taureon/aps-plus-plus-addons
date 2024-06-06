71ce57b8be2105ccb9c8e8f0f41d021e483a6d03
Zephi <93544652+Zephiii@users.noreply.github.com>
2024-06-06 08:41:03 +0000

    Update woomy.js

--- WIP/woomy.js
+++ WIP/woomy.js
@@ -1,4 +1,4 @@
-const { dereference, combineStats, makeMulti, makeAuto } = require('../facilitators.js');
+const { dereference, combineStats, makeAuto } = require('../facilitators.js');
 const { base, gunCalcNames, statnames } = require('../constants.js');
 
 module.exports = ({ Config }) => {
@@ -13,6 +13,27 @@ Config.WELCOME_MESSAGE = "You have spawned! Welcome to the game. Hold N to level
                     +"You will be invulnerable until you move, shoot, or your timer runs out."
 
 // Prerequisites
+const makeMulti = (type, count, name = -1, startRotation = 0) => {
+    type = ensureIsClass(type);
+    let greekNumbers = ',Double ,Triple ,Quad ,Penta ,Hexa ,Septa ,Octo ,Nona ,Deca ,Hendeca ,Dodeca ,Trideca ,Tetradeca ,Pentadeca ,Hexadeca ,Septadeca ,Octadeca ,Nonadeca ,Icosa ,Henicosa ,Doicosa ,Triaicosa ,Tetraicosa ,Pentaicosa ,Hexaicosa ,Septaicosa ,Octoicosa ,Nonaicosa ,Triaconta '.split(','),
+        output = dereference(type),
+        fraction = 360 / count;
+    output.GUNS = [];
+    for (let gun of type.GUNS) {
+        for (let i = 0; i < count; i++) {
+            let newgun = dereference(gun);
+            if (Array.isArray(newgun.POSITION)) {
+                newgun.POSITION[5] += startRotation + fraction * i;
+            } else {
+                newgun.POSITION.ANGLE = (newgun.POSITION.ANGLE ?? 0) + startRotation + fraction * i;
+            }
+            if (gun.PROPERTIES) newgun.PROPERTIES = gun.PROPERTIES;
+            output.GUNS.push(newgun);
+        };
+    }
+    output.LABEL = name == -1 ? (greekNumbers[count - 1] || (count + ' ')) + type.LABEL : name;
+    return output;
+}
 const statNames = statnames
 const g = {
     blank: {
